4befd4e5853627f0bdf912e68c6ed04566fe92f5
Dan <danwalsh450@gmail.com>
2020-08-06 16:11:04 +0100

    Re-Written from scracth :)

--- Ray casting 10.1.1.1.py
+++ Ray casting 10.1.1.1.py
@@ -9,10 +9,32 @@ with contextlib.redirect_stdout(None):
 pool = Pool(os.cpu_count())
 
 
+def draw(screen, segments):
+    for seg in segments:
+        pygame.draw.line(screen, [0,0,255], [seg[0][0], seg[0][1]],[seg[1][0], seg[1][1]],1)
+
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            return pygame.mouse.get_pos()
+    return None
+
+
+def addTile(segments, Mouse):
+    segments.append([[Mouse[0]-25, Mouse[1]-25], [Mouse[0]+25, Mouse[1]-25]])
+    segments.append([[Mouse[0]+25, Mouse[1]-25], [Mouse[0]+25, Mouse[1]+25]])
+    segments.append([[Mouse[0]+25, Mouse[1]+25], [Mouse[0]-25, Mouse[1]+25]])
+    segments.append([[Mouse[0]-25, Mouse[1]+25], [Mouse[0]-25, Mouse[1]-25]])
+    return segments
+
 
 if __name__ == "__main__":
     pygame.init()
-    canvas = pygame.display.set_mode([1280,720])
+    screen = pygame.display.set_mode([1280, 720])
     pygame.display.set_caption("Ray casting Engine")
     font=pygame.font.SysFont("Courier", 20, bold=True)
     clock = pygame.time.Clock()
@@ -24,3 +46,14 @@ if __name__ == "__main__":
         [[1280,-1], [1280,720]],
         [[1280,720], [-1,720]],
         [[-1,720], [-1,-1]]]
+
+    while True:
+        Mouse = events()
+        if Mouse is not None:
+            segments = addTile(segments, Mouse)
+
+        draw(screen, segments)
+
+        screen.fill([0,0,0])
+        pygame.display.flip()
+        clock.tick()
